4ef4e39c58b8c0b2cd93847916e9db09dbc407d0
Hubert Drogosz <hubertus21@gmail.com>
2020-06-02 17:37:09 +0200

    Added site description

--- src/Main.js
+++ src/Main.js
@@ -6,8 +6,9 @@ function Main() {
     <h1 className="w3-xxxlarge w3-text-red"><b>Scyzoryk KKM</b></h1>
     <hr style={{width:'50px',border:'5px solid teal'}} className="w3-round"/>
       
-    <p>Apka, która pokazuje, że coś umiem w React'cie, przy okazji nawet się może przydać</p>
-    <p>W sumie tyle, link do GitHuba: </p>
+    <p>Apka, która pokazuje, że coś tam umiem w React'cie, przy okazji nawet się może przydać.</p>
+    <p>Ogólnie do końca czerwca 2020, będą działać wszystkie karty, później kart bez zdjęcia i tych z PESELem, nie będzie można doładować. Pogooglałem trochę i się dowiedziałem, że takie karty wydawano do sierpnia 2014. I korzystając ze strony MPK, można sprawdzić pierwszy zakup biletu. Tylko tyle i aż tyle. Ale niestety sprawdzarka na stronie MPK jest słaba i mówi, że nie trzeba wymieniać gdy trzeba.</p>
+    <p>W sumie tyle, link do <a href="https://github.com/hubertus21/simple-kkm-app">GitHuba</a>. </p>
   </div>
     );
 }
